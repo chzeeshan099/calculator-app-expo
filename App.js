11414827ee265eb36c5b1f8db8a3f08ab9ab5e3b
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2025-12-14 15:34:34 +0500

    install tailwind and nativewind

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1 bg-black">
+        <View className="flex-1 items-center justify-center bg-black">
+          <Text className="text-4xl font-bold text-white">
+            Welcome to Nativewind!
+          </Text>
+        </View>
+        <StatusBar style="light" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e31919ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+    ],
+  presets: [require("nativewind/preset")],
+  darkMode: "class", // 👈 YE ZAROORI HAI
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
