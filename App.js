0b412309b6a4a592e974f987cf0fc4853f19ba07
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-02-13 10:54:22 +0500

    update code

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import SignupScreen from "./src/screens/SignupScreen";
-import HomeScreen from "./src/screens/HomeScreen";
 import CalculatorScreen from "./src/screens/CalculatorScreen";
 import { HistoryProvider } from "./src/context/HistoryContext";
 import "./global.css";
@@ -25,7 +24,6 @@ export default function App() {
               <Stack.Screen name="Calculator" component={CalculatorScreen} />
               <Stack.Screen name="Login" component={LoginScreen} />
               <Stack.Screen name="Signup" component={SignupScreen} />
-              <Stack.Screen name="Home" component={HomeScreen} />
             </Stack.Navigator>
           </SafeAreaView>
         </NavigationContainer>
