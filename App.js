f7e66eeaee68469e1d30a5554da8aac370f86be0
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-02-15 23:47:25 +0500

    remove login and sign up pages

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./src/screens/LoginScreen";
-import SignupScreen from "./src/screens/SignupScreen";
 import CalculatorScreen from "./src/screens/CalculatorScreen";
 import { HistoryProvider } from "./src/context/HistoryContext";
 import "./global.css";
@@ -22,8 +19,6 @@ export default function App() {
               screenOptions={{ headerShown: false }}
             >
               <Stack.Screen name="Calculator" component={CalculatorScreen} />
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Signup" component={SignupScreen} />
             </Stack.Navigator>
           </SafeAreaView>
         </NavigationContainer>
